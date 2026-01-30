848cf0d9c5319ae50410f2f7e9f77b1200b39848
Ilario Parato <paratoilario@icloud.com>
2026-01-30 18:44:09 +0100

    Fix auth e database

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -10,10 +10,11 @@ export async function GET(request: Request) {
 
   if (code) {
     const supabase = await createClient()
+    
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     
     if (!error) {
-      return NextResponse.redirect(`${origin}/auth/login?success=${encodeURIComponent(dict.auth.login.emailConfirmed)}`)
+      return NextResponse.redirect(`${origin}/dashboard`)
     }
   }
 
